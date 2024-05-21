4d40aa65cfb2f236938cca5543b131b1aa43c53f
SarthakAgarwal28 <132089754+SarthakAgarwal28@users.noreply.github.com>
2024-05-21 19:10:30 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Exercise 1: Create a Jupyter Notebook
+# Create a new Jupyter notebook called DataScienceEcosystem.ipynb
+
+# # Exercise 2: Create markdown cell with title of the notebook
+
+# Data Science Tools and Ecosystem
+
+# # Exercise 3 - Create a markdown cell for an introduction
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+#  Objectives:
+# - List popular languages that Data Scientists use.
+# - List commonly used libraries used by Data Scientists.
+# - Comment on Data Science tools.
+
+# # Exercise 4 - Create a markdown cell to list data science languages
+
+#  Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Java
+# 5. Julia
+# 6. Scala
+# 7. C/C++
+# 8. JavaScript
+
+# # Exercise 5 - Create a markdown cell to list data science libraries
+
+# In[ ]:
+
+
+Some of the commonly used libraries used by Data Scientists include:
+1. TensorFlow
+2. NumPy
+3. SciPy
+4. Pandas
+5. Matplotlib
+6. Keras
+7. SciKit-Learn
+8. PyTorch
+9. OpenCV
+10. BeautifulSoup
+11. Pycaret
+12. PyBrain
+13. Scrapy
+14. Plotly
+15. Seaborn
+
+
+# # Exercise 6 - Create a markdown cell with a table of Data Science tools
+
+# <table style="width:100%", align= right>
+#   <tr>
+#     <th>No.</th>
+#     <th>Data Science Tools</th>
+#     <th>Uses</th>
+#   </tr>
+#   <tr>
+#     <td>1. </td>
+#     <td>SAS(Statistical Software) </td>
+#     <td>Statistical analysis, advanced analytics, and predictive modeling</td>
+#   </tr>
+#   <tr>
+#     <td>2. </td>
+#     <td>Apache Spark</td>
+#     <td>Big data processing, distributed computing, real-time analytics</td>
+#   </tr>
+#   <tr>
+#     <td>3. </td>
+#     <td>Apache Hadoop</td>
+#     <td>Big data processing, distributed computing, realtime analytics</td>
+#   </tr>
+#   <tr>
+#     <td>4. </td>
+#     <td>Tableau </td>
+#     <td> Data visualization, dashboards, business intelligence</td>
+#   </tr>
+#   <tr>
+#     <td>5. </td>
+#     <td>Microsoft Power BI </td>
+#     <td>Self-service analytics, business intelligence, data visualization</td>
+#   </tr>
+# </table>
+
+# # Exercise 7 - Create a markdown cell introducing arithmetic expression examples
+
+# <h3>Below are a few examples of evaluating arithmetic expressions in Python.</h3>
+
+# In[11]:
+
+
+code =(25 + 15)/2
+print(code)
+
+
+# In[12]:
+
+
+code1 = (2 + 5) * 6
+print(code1)
+
+
+# In[9]:
+
+
+result = ((3 + 5) * 6) / 2
+print(result)
+
+
+# # Exercise 8 - Create a code cell to multiply and add numbers.(2 pts)
+
+# In[15]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# # Exercise 9 - Create a code cell to convert minutes to hours. (2 pts)
+
+# In[16]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+
+days = 0
+hours = 0
+mins = 0
+
+time = 200
+#days = time / 1440
+leftover_minutes = time % 1440
+hours = leftover_minutes / 60
+#mins = time - (days*1440) - (hours*60)
+print(str(days) + " days, " + str(hours) + " hours, " + str(mins) +  " mins. ")
+
+
+# # Exercise 10 -Insert a markdown cell to list objectives.
+
+# Done above
+
+# # Exercise 11 - Create a markdown cell to indicate the Author’s name.
+
+# <h3>Author</h3>
+# SARTHAK AGARWAL
